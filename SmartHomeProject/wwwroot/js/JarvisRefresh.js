52196b4a051cb16bb1e6737af23591ae7f9ac16a
Dave2002 <32719183+Dave2002@users.noreply.github.com>
2020-07-17 21:05:07 +0200

    Offline Device Handling

--- SmartHomeProject/wwwroot/js/JarvisRefresh.js
+++ SmartHomeProject/wwwroot/js/JarvisRefresh.js
@@ -8,10 +8,9 @@ function refreshDevice() {
 
 function success(data) {
 
-    console.log(data[1].onlineStatus);
-    
-    for (i = 0; i < data.length; i++) {
 
+
+    for (i = 0; i < data.length; i++) {
 
 
         if (data[i].deviceFunctions == null) {
@@ -20,26 +19,58 @@ function success(data) {
         else {
             for (k = 0; k < data[i].deviceFunctions.length; k++) {
 
-               
+
 
                 if (data[i].deviceFunctions[k].status == true) {
-                    var id = "(" + data[i].name + data[i].deviceFunctions[k].functionID + ")" + "card";
-                    document.getElementById(id).className = "card text-white bg-success mb-3";
-                    var id = "(" + data[i].name + data[i].deviceFunctions[k].functionID + ")" + "button";
-                    document.getElementById(id).className = "btn btn-danger";
-                    document.getElementById(id).value = "Ausschalten";
+                    if (data[i].onlineStatus == false) {
+                        var id = "(" + data[i].name + data[i].deviceFunctions[k].functionID + ")" + "button";
+                        document.getElementById(id).className = "btn btn-info";
+                        document.getElementById(id).value = "OFFLINE";
+                        document.getElementById(id).disabled = true;
+                        var id = "(" + data[i].name + data[i].deviceFunctions[k].functionID + ")" + "card";
+                        document.getElementById(id).className = "card text-white bg-danger mb-3";
+                    }
+                    else
+                    {
+                        var id = "(" + data[i].name + data[i].deviceFunctions[k].functionID + ")" + "card";
+                        document.getElementById(id).className = "card text-white bg-success mb-3";
+                        var id = "(" + data[i].name + data[i].deviceFunctions[k].functionID + ")" + "button";
+                        document.getElementById(id).className = "btn btn-danger";
+                        document.getElementById(id).value = "Ausschalten";
+                    }
+
                 }
                 else {
                     var id = "(" + data[i].name + data[i].deviceFunctions[k].functionID + ")" + "card";
                     document.getElementById(id).className = "card text-white bg-danger mb-3";
-                    var id = "(" + data[i].name + data[i].deviceFunctions[k].functionID + ")" + "button";
-                    document.getElementById(id).className = "btn btn-success";
-                    document.getElementById(id).value = "Einschalten";
+                    if (data[i].onlineStatus == false) {
+                        var id = "(" + data[i].name + data[i].deviceFunctions[k].functionID + ")" + "button";
+                        document.getElementById(id).className = "btn btn-info";
+                        document.getElementById(id).value = "OFFLINE";
+                        document.getElementById(id).disabled = true;
+
+                    }
+                    else {
+                        var id = "(" + data[i].name + data[i].deviceFunctions[k].functionID + ")" + "button";
+                        document.getElementById(id).className = "btn btn-success";
+                        document.getElementById(id).value = "Einschalten";
+                    }
+
                 }
 
 
 
             }
+
+        }
+
+        if (data[i].onlineStatus == false) {
+            console.log(data[i].onlineStatus);
+            var id = "(" + data[i].name + ")" + "head";
+            console.log(id)
+            document.getElementById(id).innerHTML = data[i].name + " OFFLINE!!";
+
+
         }
 
     }
