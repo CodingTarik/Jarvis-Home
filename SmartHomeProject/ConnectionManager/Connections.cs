9c8a8b93a5aebfe39681941d478a41a9ed185667
Dave2002 <32719183+Dave2002@users.noreply.github.com>
2020-07-19 22:36:43 +0200

    Connection send PY

--- SmartHomeProject/ConnectionManager/Connections.cs
+++ SmartHomeProject/ConnectionManager/Connections.cs
@@ -25,7 +25,39 @@ namespace SmartHomeProject.Connections
 
 
 
-        public void SendMessage(string operation, byte pin, int port)
+        public void SendMessage(string operation, string pin, int port)
+        {
+            try
+            {
+                TcpClient client = new TcpClient(this._ip, this._port);
+
+                string message = operation + ":" + pin + ":" + port;
+
+                int byteCount = Encoding.ASCII.GetByteCount(message);
+
+                byte[] sendDater = new byte[byteCount];
+
+                sendDater = Encoding.ASCII.GetBytes(message);
+
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(sendDater, 0, sendDater.Length);
+
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                if (Logger.Logger.VERBOSE_LOG)
+                {
+                    Logger.Logger.logError(Logger.Logger.Category.NETWORK, ex.Message, ex);
+                }
+            }
+
+
+
+        }
+        public void SendPin(string operation, byte pin, int port)
         {
             try
             {
@@ -84,7 +116,7 @@ namespace SmartHomeProject.Connections
                         {
                             ASCIIEncoding ascii = new ASCIIEncoding();
                             TcpListener listener = new TcpListener(IPAddress.Any, port);
-                            SendMessage(operation, pin, port);
+                            SendPin(operation, pin, port);
                             listener.Start();
                             TcpClient client = listener.AcceptTcpClient();
                             NetworkStream stream = client.GetStream();
@@ -109,7 +141,56 @@ namespace SmartHomeProject.Connections
 
         }
 
+        public bool recvSensor(string operation, string method)
+        {
 
+
+            try
+            {
+                int port = 456;
+                bool isAvailable = false;
+                var rand = new Random();
+                IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+                TcpConnectionInformation[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+                while (isAvailable == false)
+                {
+                    port = rand.Next(1024, 65535);
+
+                    foreach (TcpConnectionInformation tcpi in tcpConnInfoArray)
+                    {
+                        if (tcpi.LocalEndPoint.Port == port)
+                        {
+                            isAvailable = false;
+                            return false;
+                        }
+                        else
+                        {
+                            ASCIIEncoding ascii = new ASCIIEncoding();
+                            TcpListener listener = new TcpListener(IPAddress.Any, port);
+                            SendMessage(operation, method, port);
+                            listener.Start();
+                            TcpClient client = listener.AcceptTcpClient();
+                            NetworkStream stream = client.GetStream();
+                            byte[] bytes = new byte[1024];
+                            stream.Read(bytes, 0, bytes.Length);
+                            stream.Close();
+                            client.Close();
+                            return BitConverter.ToInt32(bytes) == 49;
+
+                        }
+                    }
+                }
+                return false;
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+        }
 
 
 
